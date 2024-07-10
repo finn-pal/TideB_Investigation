cc5565cdea7d3b47783d9e328831bd186165ae8e
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-07-10 16:33:36 +1000

    Modified formator settings to include notebooks

---
+++ test.py
@@ -0,0 +1,14 @@
+import glob
+import os
+import re
+import struct
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pynbody
+import pynbody.plot as pp
+import pynbody.plot.sph as sph
+import scipy.interpolate
+import scipy.signal
